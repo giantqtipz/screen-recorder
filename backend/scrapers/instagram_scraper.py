cb78f6601f3a1ae3e97c35cd08a679951366bd39
Abimanyu Saridjo <abimanyusaridjo@Abimanyus-MBP.lan>
2022-02-23 10:19:25 -0500

    code cleanup

--- backend/scrapers/instagram_scraper.py
+++ backend/scrapers/instagram_scraper.py
@@ -1,18 +1,12 @@
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 from recorder.recorder import record
-
 import time
 
-def instagram_scraper(url, duration):
-
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+def instagram_scraper(driver, url, duration):
 
     driver.implicitly_wait(5)
+
     driver.get(url)
-    driver.maximize_window()
 
     play_button_xpath = '/html/body/div[1]/section/main/div/div/article/div/div[1]/div/div/div[3]'
     play_button_click = driver.find_element(By.XPATH, play_button_xpath).click()
@@ -25,11 +19,8 @@ def instagram_scraper(url, duration):
     instagram_handle_xpath = '/html/body/div[1]/section/main/div/div/article/div/div[2]/div/div[1]/div/header/div[2]/div[1]/div[1]/div'
     instagram_handle_text = driver.find_element(By.XPATH, instagram_handle_xpath).text
 
-    record(instagram_handle_text, duration)
-
     time.sleep(duration)
 
-    # return instagram_handle_text
+    record(instagram_handle_text, duration)
 
-    # driver.close()
-    # driver.quit()
+    # return instagram_handle_text
--- backend/scrapers/tiktok_scraper.py
+++ backend/scrapers/tiktok_scraper.py
@@ -1,18 +1,12 @@
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 from recorder.recorder import record
-
 import time
 
-def tiktok_scraper(url, duration):
-
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+def tiktok_scraper(driver, url, duration):
 
     driver.implicitly_wait(5)
+
     driver.get(url)
-    driver.maximize_window()
 
     close_button_xpath = '/html/body/div[7]/div/div[1]/div[1]'
     driver.find_element(By.XPATH, close_button_xpath).click()
@@ -21,17 +15,15 @@ def tiktok_scraper(url, duration):
     driver.find_element(By.XPATH, video_xpath).click()
 
     driver.implicitly_wait(5)
+
     tiktok_handle_xpath = '/html/body/div[2]/div[2]/div[3]/div[2]/div[1]/a[2]/span[1]'
     tiktok_handle_text = driver.find_element(By.XPATH, tiktok_handle_xpath).text
 
     # tiktok_nickname_xpath = '/html/body/div[2]/div[2]/div[3]/div[2]/div[1]/a[2]/span[2]'
     # tiktok_nickname_text = driver.find_element(By.XPATH, tiktok_nickname_xpath).text
 
-    record(tiktok_handle_text, duration)
-
     time.sleep(duration)
 
-    # driver.close()
-    # driver.quit()
+    record(tiktok_handle_text, duration)
 
     # return tiktok_handle_text
